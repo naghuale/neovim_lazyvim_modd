3a8caad1ff87f19ac9c7db2b993f853764fe8368
naghuale <93920024+naghuale@users.noreply.github.com>
2023-02-24 07:43:44 +1000

    Loaded config

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -3,6 +3,16 @@ return {
     "storyn26383/vim-vue",
   },
   {
+    "preservim/tagbar",
+  },
+  {
+    "simnalamburt/vim-mundo",
+    cmd = {
+      "MundoToggle",
+      "MundoShow",
+    },
+  },
+  {
     "axelvc/template-string.nvim",
     event = "InsertEnter",
     ft = { "javascript", "typescript", "javascriptreact", "typescriptreact", "python" },
